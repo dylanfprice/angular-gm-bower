314c7952f3400e05c3acd5ce6f9b22a586357965
Dylan Price <the.dylan.price@gmail.com>
2017-01-04 14:09:11 -0800

    2.0.0

--- angular-gm.js
+++ angular-gm.js
@@ -1,6 +1,6 @@
 /**
  * AngularGM - Google Maps Directives for AngularJS
- * @version v1.0.2 - 2015-06-17
+ * @version v2.0.0 - 2017-01-04
  * @link http://dylanfprice.github.com/angular-gm
  * @author Dylan Price <the.dylan.price@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -40,7 +40,7 @@
    *       'markerConstructor': myCustomMarkerConstructor,
    *       'polylineConstructor': myCustomPolylineConstructor,
    *       'mapOptions': {
-   *         center: new google.maps.LatLng(55, 111),
+   *         center: {lat: 55, lng: 111},
    *         mapTypeId: google.maps.MapTypeId.SATELLITE,
    *         ...
    *       }
@@ -57,13 +57,159 @@
       'circleConstructor': google.maps.Circle,
       'mapOptions': {
         zoom : 8,
-        center : new google.maps.LatLng(46, -120),
+        center : {lat: 46, lng: -120},
         mapTypeId : google.maps.MapTypeId.ROADMAP
       }
     };
   });
 
 })();
+
+/**
+ * @ngdoc directive
+ * @name angulargm.directive:gmCircles
+ * @element ANY
+ *
+ * @description
+ * A directive for adding circles to a `gmMap`. You may have multiple per `gmMap`.
+ *
+ * To use, you specify an array of custom objects and tell the directive how to
+ * extract the center point for the circle from them. A circle will be created
+ * for each of your objects. If you assign a new array to your scope variable
+ * or change the array's length, the circles will also update.  The one case
+ * where `gmCircles` can not automatically detect changes to your objects is
+ * when you mutate objects in the array. To inform the directive of such
+ * changes, see the `gmCirclesUpdate` event below.
+ *
+ * Only the `gm-objects`, `gm-id` and `gm-circle-center` attributes are required.
+ *
+ * @param {expression} gm-objects an array of objects in the current scope.
+ * These can be any objects you wish to attach to circles, the only requirement
+ * is that they have a uniform method of accessing an id and a center.
+ *
+ * @param {expression} gm-circle-center an angular expression that given an
+ * object from `gm-objects`, evaluates to a google.maps.LatLngLiteral object.
+ * Your object can be accessed through the variable `object`.  For example, if
+ * your controller has
+ * ```js
+ * ...
+ * $scope.myObjects = [
+ *   { id: 0, center: { lat: 5, lng: 5}},
+ *   { id: 1, center: { lat: 6, lng: 6}}
+ * ]
+ * ...
+ * ```
+ * then in the `gm-circles` directive you would put
+ * ```js
+ * ...
+ * gm-objects="myObjects"
+ * gm-circle-center="object.center"
+ * ...
+ * ```
+ *
+ * @param {expression} gm-circle-options an angular expression that given
+ * an object from `gm-objects`, evaluates to a
+ * [google.maps.CircleOptions](https://developers.google.com/maps/documentation/javascript/reference#CircleOptions)
+ * object.  Your object can be accessed through the variable `object`. If
+ * unspecified, google maps api defaults will be used.
+ *
+ * @param {expression} gm-events a variable in the current scope that is used to
+ * simulate events on circles. Setting this variable to an object of the form
+ * ```js
+ *     [
+ *       {
+ *         event: 'click',
+ *         ids: [id1, ...]
+ *       },
+ *       ...
+ *     ]
+ * ```
+ * will generate the named events on the circles with the given ids, if a
+ * circle with each id exists. Note: when setting the `gm-events` variable, you
+ * must set it to a new object for the changes to be detected.  Code like
+ * ```js
+ * myEvents[0]["ids"] = [0]
+ * ```
+ * will not work.
+ *
+ *
+ * @param {expression} gm-on-*event* an angular expression which evaluates to
+ * an event handler. This handler will be attached to each circle's \*event\*
+ * event.  The variables 'object' and 'circle' evaluate to your object and the
+ * [google.maps.circle](https://developers.google.com/maps/documentation/javascript/reference#Circle),
+ * respectively. For example:
+ * ```html
+ * gm-on-click="myClickFn(object, circle)"
+ * ```
+ * will call your `myClickFn` whenever a circle is clicked.  You may have
+ * multiple `gm-on-*event*` handlers, but only one for each type of event.
+ * For events that have an underscore in their name, such as
+ * 'position_changed', write it as 'gm-on-position-changed'.
+ */
+
+/**
+ * @ngdoc event
+ * @name angulargm.directive:gmCircles#gmCirclesUpdate
+ * @eventOf angulargm.directive:gmCircles
+ * @eventType listen on current gmCircles scope
+ *
+ * @description Manually tell the `gmCircles` directive to update the circles.
+ * This is useful to tell the directive when an object from `gm-objects` is
+ * mutated--`gmCircles` can not pick up on such changes automatically.
+ *
+ * @param {string} objects Not required. The name of the scope variable which
+ * holds the objects to update circles for, i.e. what you set `gm-objects` to.
+ * It is useful because there may be multiple instances of the `gmCircles`
+ * directive. If not specified, all instances of `gmCircles` which are child
+ * scopes will update their circles.
+ *
+ * @example
+ * ```js
+ * $scope.$broadcast('gmCirclesUpdate', 'myObjects');
+ * ```
+ */
+
+/**
+ * @ngdoc event
+ * @name angulargm.directive:gmCircles#gmCirclesRedraw
+ * @eventOf angulargm.directive:gmCircles
+ * @eventType listen on current gmCircles scope
+ *
+ * @description Force the gmCircles directive to clear and redraw all circles.
+ *
+ * @param {string} objects Not required. The name of the scope variable which
+ * holds the objects to redraw circles for, i.e. what you set `gm-objects` to.
+ * It is useful because there may be multiple instances of the `gmCircles`
+ * directive. If not specified, all instances of gmCircles which are child
+ * scopes will redraw their circles.
+ *
+ * @example
+ * ```js
+ * $scope.$broadcast('gmCirclesRedraw', 'myObjects');
+ * ```
+ */
+
+/**
+ * @ngdoc event
+ * @name angulargm.directive:gmCircles#gmCirclesUpdated
+ * @eventOf angulargm.directive:gmCircles
+ * @eventType emit on current gmCircles scope
+ *
+ * @description Emitted when circles are updated.
+ *
+ * @param {string} objects the name of the scope variable which holds the
+ * objects the gmCircles directive was constructed with. This is what
+ * `gm-objects` was set to.
+ *
+ * @example
+ * ```js
+ * $scope.$on('gmCirclesUpdated', function(event, objects) {
+ *     if (objects === 'myObjects') {
+ *       ...
+ *     }
+ * });
+ * ```
+ */
 
 (function () {
     'use strict';
@@ -73,7 +219,7 @@
   directive('gmCircles', ['$parse', '$compile', '$timeout', '$log', 'angulargmUtils', 'angulargmShape',
     function ($parse, $compile, $timeout, $log, angulargmUtils, angulargmShape) {
      
-        var objToLatLng = angulargmUtils.objToLatLng;
+        var validateLatLng = angulargmUtils.validateLatLng;
 
         function link(scope, element, attrs, controller) {
             if (!('gmCircleCenter' in attrs)) {
@@ -82,7 +228,7 @@
 
             var circleOptions = function (object) {
                 var latLngObj = scope.gmCircleCenter({ object: object });    
-                var center = objToLatLng(latLngObj);
+                var center = validateLatLng(latLngObj);
                 if (center == null) {
                     return null;
                 }
@@ -111,6 +257,7 @@
         };
     }]);
 })();
+
 /**
  * @ngdoc directive
  * @name angulargm.directive:gmInfoWindow
@@ -214,7 +361,7 @@
  *
  *
  * @param {expression} gm-center center variable in the current scope.  The
- * value will be a google.maps.LatLng object.
+ * value will be a google.maps.LatLngLiteral object.
  *
  *
  * @param {expression} gm-zoom zoom variable in the current scope.  Value will
@@ -295,6 +442,8 @@
 
     /** aliases **/
     var getEventHandlers = angulargmUtils.getEventHandlers;
+    var validateLatLng = angulargmUtils.validateLatLng;
+    var latLngLiteralEqual = angulargmUtils.latLngLiteralEqual;
 
     /** link function **/
     function link(scope, element, attrs, controller) {
@@ -392,11 +541,12 @@
 
       if (hasCenter) {
         scope.$watch('gmCenter', function (newValue, oldValue) {
-          var changed = (newValue !== oldValue);
+          var changed = (
+              validateLatLng(newValue) != null &&
+              !latLngLiteralEqual(newValue, oldValue)
+          );
           if (changed && !controller.dragging) {
-            var latLng = newValue;
-            if (latLng)
-              controller.center = latLng;
+              controller.center = newValue;
           }
         }, true);
       }
@@ -493,10 +643,10 @@
  * can be accessed through the variable `object`. See `gm-position` below for
  * an example.
  *
- * @param {expression} gm-position an angular expression that given an object from
- * `gm-objects`, evaluates to an object with lat and lng properties. Your
- * object can be accessed through the variable `object`.  For example, if
- * your controller has
+ * @param {expression} gm-position an angular expression that given an object
+ * from `gm-objects`, evaluates to google.maps.LatLngLiteral object. Your
+ * object can be accessed through the variable `object`.  For example, if your
+ * controller has
  * ```js
  * ...
  * $scope.myObjects = [
@@ -510,7 +660,7 @@
  * ...
  * gm-objects="myObjects"
  * gm-id="object.id"
- * gm-position="{ lat: object.location.lat, lng: object.location.lng }"
+ * gm-position="object.location"
  * ...
  * ```
  *
@@ -630,7 +780,7 @@
     function($log, $parse, $timeout, angulargmUtils, angulargmShape) {
 
     /** aliases */
-    var objToLatLng = angulargmUtils.objToLatLng;
+    var validateLatLng = angulargmUtils.validateLatLng;
 
     function link(scope, element, attrs, controller) {
       // check marker attrs
@@ -640,7 +790,7 @@
 
       var markerOptions = function(object) {
         var latLngObj = scope.gmPosition({object: object});
-        var position = objToLatLng(latLngObj);
+        var position = validateLatLng(latLngObj);
         if (position == null) {
           return null;
         }
@@ -694,10 +844,10 @@
  * These can be any objects you wish to attach to polylines, the only requirement
  * is that they have a uniform method of accessing an id and a path.
  *
- * @param {expression} gm-path an angular expression that given an object
- * from `gm-objects`, evaluates to an array of objects with lat and lng
- * properties. Your object can be accessed through the variable `object`.  For
- * example, if your controller has
+ * @param {expression} gm-path an angular expression that given an object from
+ * `gm-objects`, evaluates to an array of google.maps.LatLngLiteral objects.
+ * Your object can be accessed through the variable `object`.  For example, if
+ * your controller has
  * ```js
  * ...
  * $scope.myObjects = [
@@ -826,7 +976,7 @@
   directive('gmPolylines', ['$parse', '$compile', '$timeout', '$log', 'angulargmUtils', 'angulargmShape',
     function ($parse, $compile, $timeout, $log, angulargmUtils, angulargmShape) {
     /** aliases */
-    var objToLatLng = angulargmUtils.objToLatLng;
+    var validateLatLng = angulargmUtils.validateLatLng;
 
     function link(scope, element, attrs, controller) {
       if (!('gmPath' in attrs)) {
@@ -838,7 +988,7 @@
         var path = [];
 
         angular.forEach(lineLatLngs, function(latlng) {
-          var position = objToLatLng(latlng);
+          var position = validateLatLng(latlng);
           if (position == null) {
               $log.warn('Unable to generate lat/lng from ', latlng);
               return;
@@ -1241,6 +1391,26 @@
 
     /**
      * @ngdoc function
+     * @name #latLngLiteralEqual
+     * @methodOf angulargm.service:angulargmUtils
+     *
+     * @param {google.maps.LatLngLiteral} l1 first
+     * @param {google.maps.LatLngLiteral} l2 second
+     * @return {boolean} true if l1 and l2 are 'very close'. If either are null
+     * or not google.maps.LatLngLiteral objects returns false.
+     */
+    function latLngLiteralEqual(l1, l2) {
+      if (
+        !(l1 != null && l1.hasOwnProperty('lat') && l1.hasOwnProperty('lng')) ||
+        !(l2 != null && l2.hasOwnProperty('lat') && l2.hasOwnProperty('lng'))
+      ) {
+        return false;
+      }
+      return floatEqual(l1.lat, l2.lat) && floatEqual(l1.lng, l2.lng);
+    }
+
+    /**
+     * @ngdoc function
      * @name #boundsEqual
      * @methodOf angulargm.service:angulargmUtils
      *
@@ -1305,6 +1475,30 @@
 
     /**
      * @ngdoc function
+     * @name #validateLatLng
+     * @methodOf angulargm.service:angulargmUtils
+     *
+     * @param {Object} obj of the form { lat: 40, lng: -120 }
+     * @return {Object} obj or returns null if problems with obj (null,
+     * NaN, etc.)
+     */
+    function validateLatLng(obj) {
+      if (obj != null) {
+        var lat = obj.lat;
+        var lng = obj.lng;
+        var ok = (
+          !(lat == null || lng == null) && 
+          !(isNaN(lat) || isNaN(lng))
+        );
+        if (ok) {
+          return obj;
+        }
+      }
+      return null;
+    }
+
+    /**
+     * @ngdoc function
      * @name #hasNaN
      * @methodOf angulargm.service:angulargmUtils
      *
@@ -1355,9 +1549,11 @@
 
     return {
       latLngEqual: latLngEqual,
+      latLngLiteralEqual: latLngLiteralEqual,
       boundsEqual: boundsEqual,
       latLngToObj: latLngToObj,
       objToLatLng: objToLatLng,
+      validateLatLng: validateLatLng,
       hasNaN: hasNaN,
       getEventHandlers: getEventHandlers,
       assertDefined: assertDefined
@@ -1427,7 +1623,8 @@
       angulargmContainer) {
 
     /** aliases */
-    var latLngEqual = angulargmUtils.latLngEqual;
+    var latLngToObj = angulargmUtils.latLngToObj;
+    var validateLatLng = angulargmUtils.validateLatLng;
     var boundsEqual = angulargmUtils.boundsEqual;
     var hasNaN = angulargmUtils.hasNaN;
     var assertDefined = angulargmUtils.assertDefined;
@@ -1465,12 +1662,12 @@
         'center': {
           configurable: true, // for testing so we can mock
           get: function() {
-             return this._map.getCenter();
+             return latLngToObj(this._map.getCenter());
            },
           set: function(center) {
-            if (hasNaN(center))
+            if (validateLatLng(center) === null)
               throw 'center contains null or NaN';
-            var changed = !latLngEqual(this.center, center);
+            var changed = this.center !== center;
             if (changed) {
               this._map.panTo(center);
             }
@@ -1663,9 +1860,6 @@
 
     this._newElement = function(type, opts) {
         if (type === 'marker') {
-            if (!(opts.position instanceof google.maps.LatLng)) {
-              throw 'markerOptions did not contain a position';
-            }
             return new angulargmDefaults.markerConstructor(opts);
         } else if (type === 'polyline') {
             if (!(opts.path instanceof Array)) {
@@ -1673,9 +1867,6 @@
             }
             return new angulargmDefaults.polylineConstructor(opts);
         } else if (type === 'circle') {
-            if (!(opts.center instanceof google.maps.LatLng)) {
-                throw 'circleOptions did not contain a marker position';
-            }
             return new angulargmDefaults.circleConstructor(opts);
         }
         else {
